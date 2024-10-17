7f24b00451c131cb186f4822494aef7e0c3894ad
WisniaFresh <kuba.webdev@gmail.com>
2024-10-17 15:01:43 +0200

    quick fixes

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Getting Started
+## Demo
+
+[Demo link](https://nextjs-course-blog-henna-three.vercel.app/)
+
+## Getting Started with Development
 
 First, run the development server:
 
--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = () => {
-  if (PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: "user-code-with-max",
-        mongodb_password: "meNhY6ZASG1ALeHs",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "my-site-blog",
-      },
-    };
-  }
-
-  return {
-    env: {
-      mongodb_username: "user-code-with-max",
-      mongodb_password: "meNhY6ZASG1ALeHs",
-      mongodb_clustername: "cluster0",
-      mongodb_database: "my-site-blog-production",
-    },
-  };
+module.exports = {
+  // used on dev
+  env: {
+    mongodb_username: "user-code-with-max",
+    mongodb_password: "meNhY6ZASG1ALeHs",
+    mongodb_clustername: "cluster0",
+    mongodb_database: "my-site-blog",
+  },
 };
