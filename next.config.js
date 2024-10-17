91e1ec6d4d7325d472d7cd0829d34e9e49384ddd
WisniaFresh <kuba.webdev@gmail.com>
2024-10-17 15:22:49 +0200

    fix env variables

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // used on dev
-  env: {
-    mongodb_username: "user-code-with-max",
-    mongodb_password: "meNhY6ZASG1ALeHs",
-    mongodb_clustername: "cluster0",
-    mongodb_database: "my-site-blog",
-  },
-};
