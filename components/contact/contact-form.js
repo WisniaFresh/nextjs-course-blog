1f8b3d0ecbd8b3cdcc2f91c418eeb18b6011552b
WisniaFresh <kuba.webdev@gmail.com>
2024-10-17 15:51:43 +0200

    revalidate deploy

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -25,6 +25,8 @@ function ContactForm() {
   const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
   const [errorMessage, setErrorMessage] = useState("");
 
+  console.log("revalidate deploy");
+
   useEffect(() => {
     if (requestStatus === "success" || requestStatus === "error") {
       const timer = setTimeout(() => {
